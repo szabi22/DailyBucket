9bc6823f1579b1c5884626276cff33be8c6ec447
szabi22 <szabolcskiss898@gmail.comn>
2019-10-10 11:46:25 +0300

    2

---
+++ app/src/main/java/com/example/dailybucket/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.dailybucket;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanseState) {
+        super.onCreate(savedInstanseState);
+        setContentView(R.layout.login_activity);
+    }
+}
--- app/src/main/java/com/example/dailybucket/MainActivity.java
+++ app/src/main/java/com/example/dailybucket/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.dailybucket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle savedInstanseState) {
+        super.onCreate(savedInstanseState);
         setContentView(R.layout.activity_main);
     }
+    public void openActivity (View view)
+    {
+        Intent intent =new Intent (this,LoginActivity.class);
+        startActivity (intent);
+
+    }
 }
+
+
