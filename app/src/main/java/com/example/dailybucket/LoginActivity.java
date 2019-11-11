9cec8423d6ae70e5ebc30b70089d8a3606f2cb0e
szabi22 <szabolcskiss898@gmail.comn>
2019-11-11 20:27:28 +0200

    7.5

--- app/src/main/java/com/example/dailybucket/LoginActivity.java
+++ app/src/main/java/com/example/dailybucket/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.dailybucket;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,3 +15,19 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.login_activity);
     }
 }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+
+        @SuppressLint("WrongViewCast") Button button2 = findViewById(R.id.image2);
+        button2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.button2) {
+            Intent intent1 = new Intent(this, RegisterActivity.class);
+            startActivity(intent1);
+        }
